483f2c326edbddbc4e76db4695614a4c8e2f29e0
yeti <crazyyeti@mail.ru>
2021-02-22 11:38:30 +0300

    Preparing

--- 2sem1lab/cmake-build-debug/CMakeFiles/2sem1lab.dir/DependInfo.cmake
+++ 2sem1lab/cmake-build-debug/CMakeFiles/2sem1lab.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/crazy/CLionProjects/2sem1lab/main.cpp" "C:/Users/crazy/CLionProjects/2sem1lab/cmake-build-debug/CMakeFiles/2sem1lab.dir/main.cpp.obj"
+  "C:/Users/crazy/CLionProjects/itmo_proga_2sem/2sem1lab/main.cpp" "C:/Users/crazy/CLionProjects/itmo_proga_2sem/2sem1lab/cmake-build-debug/CMakeFiles/2sem1lab.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- 2sem1lab/cmake-build-debug/cmake_install.cmake
+++ 2sem1lab/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/crazy/CLionProjects/2sem1lab
+# Install script for directory: C:/Users/crazy/CLionProjects/itmo_proga_2sem/2sem1lab
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/crazy/CLionProjects/2sem1lab/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/crazy/CLionProjects/itmo_proga_2sem/2sem1lab/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
