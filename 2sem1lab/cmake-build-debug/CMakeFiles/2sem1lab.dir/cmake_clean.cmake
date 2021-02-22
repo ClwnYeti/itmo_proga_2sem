0d9f22fd5e5fd95ef596658dd483c9142c551bd7
yeti <crazyyeti@mail.ru>
2021-02-22 11:36:12 +0300

    Preparing

---
+++ 2sem1lab/cmake-build-debug/CMakeFiles/2sem1lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2sem1lab.exe"
+  "2sem1lab.exe.manifest"
+  "2sem1lab.pdb"
+  "CMakeFiles/2sem1lab.dir/main.cpp.obj"
+  "lib2sem1lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2sem1lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
