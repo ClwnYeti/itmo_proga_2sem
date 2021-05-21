6093d125b9b9153f90404f8d4a23706441da9c79
yeti <crazyyeti@mail.ru>
2021-05-21 17:27:31 +0300

    5lab

--- 2sem5lab/main.cpp
+++ 2sem5lab/main.cpp
@@ -14,189 +14,218 @@ public:
     class CircleIterator: public std::iterator<std::random_access_iterator_tag, Value_T>
     {
     private:
-        Value_T* element_;
+        CircleBuffer<Value_T>& buffer_;
+        int index_ = 0;
     public:
         CircleIterator() = default;
-        explicit CircleIterator(Value_T* el)
-        : element_(el)
+        CircleIterator(CircleBuffer<Value_T>& buffer, int index)
+        : buffer_(buffer),
+        index_((index - buffer.indexOfFirst() + buffer.capasity()) % buffer.capasity())
         {}
-        CircleIterator& operator= (const CircleIterator& other) = default;
-        bool operator== (const CircleIterator& other) const
+        CircleIterator &operator=(const CircleIterator &other) = default;
+
+        bool operator==(const CircleIterator &other) const
         {
-            element_ = other.element_;
+            return index_ == other.index_;
         }
 
-        bool operator!= (const CircleIterator& other) const
+        bool operator!=(const CircleIterator &other) const
         {
-            return !this == other;
+            return !*this == other;
         }
-        CircleIterator& operator+= (int ind)
+
+        CircleIterator &operator+=(int ind)
         {
-            ind = (ind % capacity_ + capacity_) % capacity_;
-            for (int i = 0; i < ind; i++)
-            {
-                element_++;
-                if (element_ == dataEnd_)
-                {
-                    element_ = data_;
-                }
-            }
+            index_ = ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
             return *this;
         }
-        CircleIterator& operator-= (int ind)
+
+        CircleIterator &operator-=(int ind)
         {
-            ind = (-ind % capacity_ + capacity_) % capacity_;
-            for (int i = 0; i < ind; i++)
-            {
-                element_++;
-                if (element_ == dataEnd_)
-                {
-                    element_ = data_;
-                }
-            }
-            return this;
+            index_ = ((index_ - ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
+            return *this;
         }
-        CircleIterator& operator+ (int ind) const
+
+        CircleIterator operator+(int ind) const
         {
-            CircleIterator other = this;
+            CircleIterator other = *this;
             other += ind;
             return other;
         }
-        size_t operator- (const CircleIterator& other) const
+
+        size_t operator-(const CircleIterator &other) const
         {
-            Value_T* k = *data_[firstEl_];
-            int ind1;
-            int ind2;
-            for (int i = firstEl_; i < endEl_; i++)
-            {
-                if (element_ == k)
-                {
-                    ind1 = i;
-                }
-                if (other.element_ == k)
-                {
-                    ind2 = i;
-                }
-            }
-            return (ind1 - ind2) * sizeof(Value_T);
+            return ((index_ - other.index_ + buffer_.capasity()) % buffer_.capasity()) * sizeof(Value_T);
         }
-        CircleIterator& operator++ ()
+
+        CircleIterator operator++()
         {
-            CircleIterator other = this;
-            this += 1;
+            CircleIterator other = *this;
+            operator+=(1);
             return other;
         }
-        CircleIterator& operator-- ()
+
+        CircleIterator operator--()
         {
-            CircleIterator other = this;
-            this += 1;
+            CircleIterator other = *this;
+            operator-=(1);
             return other;
         }
-        CircleIterator& operator++ (int)
+
+        CircleIterator &operator++(int)
         {
-            this += 1;
+            operator+=(1);
             return *this;
         }
-        CircleIterator& operator-- (int)
+
+        CircleIterator &operator--(int)
         {
-            this -= 1;
+            operator-=(1);
             return *this;
         }
-        CircleIterator& operator- (int ind) const
+
+        CircleIterator operator-(int ind) const
         {
-            CircleIterator other = this;
+            CircleIterator other = *this;
             other -= ind;
             return other;
         }
-        Value_T& operator[] (int ind)
+
+        Value_T &operator[](int ind)
         {
-            ind = (ind % capacity_ + capacity_) % capacity_;
-            Value_T* k = element_;
-            for (int i = 0; i < ind; i++)
-            {
-                k++;
-                if (k == dataEnd_)
-                {
-                    k = data_;
-                }
-            }
-            return *k;
+            return *(buffer_.data() + ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity());
         }
-        Value_T& operator* ()
+
+        Value_T &operator*()
         {
             return operator[](0);
         }
         bool operator> (const CircleIterator& other) const
         {
-            Value_T* k = *data_[firstEl_];
-            int ind1;
-            int ind2;
-            for (int i = firstEl_; i < endEl_; i++)
-            {
-                if (element_ == k)
-                {
-                    ind1 = i;
-                }
-                if (other.element_ == k)
-                {
-                    ind2 = i;
-                }
-            }
-            return ind1 > ind2;
-        }
-        bool operator< (const CircleIterator& other) const
-        {
-            Value_T* k = *data_[firstEl_];
-            int ind1;
-            int ind2;
-            for (int i = firstEl_; i < endEl_; i++)
-            {
-                if (element_ == k)
-                {
-                    ind1 = i;
-                }
-                if (other.element_ == k)
-                {
-                    ind2 = i;
-                }
-            }
-            return ind1 < ind2;
+            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >
+                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
         }
         bool operator>= (const CircleIterator& other) const
         {
-            Value_T* k = *data_[firstEl_];
-            int ind1;
-            int ind2;
-            for (int i = firstEl_; i < endEl_; i++)
-            {
-                if (element_ == k)
-                {
-                    ind1 = i;
-                }
-                if (other.element_ == k)
-                {
-                    ind2 = i;
-                }
-            }
-            return ind1 >= ind2;
+            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >=
+                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
+        }
+        bool operator< (const CircleIterator& other) const
+        {
+            return !operator>=(other);
         }
         bool operator<= (const CircleIterator& other) const
         {
-            Value_T* k = *data_[firstEl_];
-            int ind1;
-            int ind2;
-            for (int i = firstEl_; i < endEl_; i++)
+            return !operator>(other);
+        }
+    };
+    class CircleIteratorConst: public std::iterator<std::random_access_iterator_tag, Value_T>
             {
-                if (element_ == k)
+    private:
+        const CircleBuffer<Value_T>& buffer_;
+        int index_ = 0;
+    public:
+        CircleIteratorConst() = default;
+        CircleIteratorConst(const CircleBuffer<Value_T>& buffer, int index)
+                : buffer_(buffer),
+                  index_((index - buffer.indexOfFirst() + buffer.capasity()) % buffer.capasity())
+        {}
+        CircleIteratorConst &operator=(const CircleIteratorConst &other) = default;
+
+        bool operator==(const CircleIteratorConst &other) const
+        {
+            return index_ == other.index_;
+        }
+
+        bool operator!=(const CircleIteratorConst &other) const
+        {
+            return !*this == other;
+        }
+
+        CircleIteratorConst &operator+=(int ind)
+        {
+            index_ = ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
+            return *this;
+        }
+
+        CircleIterator &operator-=(int ind)
                 {
-                    ind1 = i;
-                }
-                if (other.element_ == k)
-                {
-                    ind2 = i;
-                }
-            }
-            return ind1 <= ind2;
+            index_ = ((index_ - ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
+            return *this;
+        }
+
+        CircleIteratorConst operator+(int ind) const
+        {
+            CircleIteratorConst other = *this;
+            other += ind;
+            return other;
+        }
+
+        size_t operator-(const CircleIteratorConst &other) const
+        {
+            return ((index_ - other.index_ + buffer_.capasity()) % buffer_.capasity()) * sizeof(Value_T);
+        }
+
+        CircleIteratorConst operator++()
+        {
+            CircleIteratorConst other = *this;
+            operator+=(1);
+            return other;
+        }
+
+        CircleIteratorConst operator--()
+        {
+            CircleIterator other = *this;
+            operator-=(1);
+            return other;
+        }
+
+        CircleIteratorConst &operator++(int)
+        {
+            operator+=(1);
+            return *this;
+        }
+
+        CircleIteratorConst &operator--(int)
+        {
+            operator-=(1);
+            return *this;
+        }
+
+        CircleIteratorConst operator-(int ind) const
+        {
+
+            CircleIteratorConst other = *this;
+            other -= ind;
+            return other;
+        }
+
+        const Value_T &operator[](int ind) const
+        {
+            return *(buffer_.data() + ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity());
+        }
+
+        const Value_T &operator*() const
+        {
+            return operator[](0);
+        }
+        bool operator> (const CircleIteratorConst& other) const
+        {
+            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >
+                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
+        }
+        bool operator>= (const CircleIteratorConst& other) const
+        {
+            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >=
+                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
+        }
+        bool operator< (const CircleIteratorConst& other) const
+        {
+            return !operator>=(other);
+        }
+        bool operator<= (const CircleIteratorConst& other) const
+        {
+            return !operator>(other);
         }
     };
     CircleBuffer() = default;
@@ -214,9 +243,9 @@ public:
     }
     bool isEmpty()
     {
-        return ((endEl_ - firstEl_) + capacity_) % capacity_ == 0;
+        return endEl_ - firstEl_ == 0;
     }
-    bool push_back(Value_T& i)
+    bool push_back(const Value_T& i)
     {
         if (isFull())
             return false;
@@ -224,19 +253,12 @@ public:
         endEl_ = (endEl_ + 1 + capacity_) % capacity_;
         return true;
     }
-    bool push_front(Value_T& i)
+    bool push_front(const Value_T& i)
     {
         if (isFull())
             return false;
-        if (isEmpty())
-        {
-            data_[firstEl_] = i;
-            endEl_ = (endEl_ + 1 + capacity_) % capacity_;
-        } else
-        {
-            firstEl_ = (firstEl_ - 1 + capacity_) % capacity_;
-            data_[firstEl_] = i;
-        }
+        firstEl_ = (firstEl_ - 1 + capacity_) % capacity_;
+        data_[firstEl_] = i;
         return true;
     }
     bool pop_front()
@@ -244,12 +266,14 @@ public:
         if (isEmpty())
             return false;
         firstEl_++;
+        return true;
     }
     bool pop_back()
     {
         if (isEmpty())
             return false;
         endEl_--;
+        return true;
     }
     Value_T& operator[](int index)
     {
@@ -259,21 +283,71 @@ public:
                 return data_[k];
         throw std::invalid_argument("Out of range");
     }
-    Value_T* first()
+    Value_T& operator[](int index) const
     {
-        return &operator[](firstEl_);
+        int k = (firstEl_ + index % capacity_ + capacity_) % capacity_;
+        if ((endEl_ > firstEl_ && firstEl_ <= k && k < endEl_) ||
+            ((endEl_ < firstEl_) && !(firstEl_ <= k && k < endEl_)))
+            return data_[k];
+        throw std::invalid_argument("Out of range");
     }
-    Value_T* end()
+    Value_T& front()
     {
-        return &operator[](endEl_);
+        return data_[firstEl_];
+    }
+    Value_T& front() const
+    {
+        return data_[firstEl_];
+    }
+    Value_T& back()
+    {
+        return data_[endEl_ - 1];
+    }
+    Value_T& back() const
+    {
+        return data_[endEl_ - 1];
+    }
+    Value_T* data() const
+    {
+        return data_;
+    }
+    int capasity() const
+    {
+        return capacity_;
+    }
+    CircleIterator& begin()
+    {
+        return CircleIterator(*this, firstEl_);
+    }
+    CircleIterator& end()
+    {
+        return CircleIterator(*this, endEl_);
+    }
+    CircleIteratorConst& begin() const
+    {
+        return CircleIteratorConst(*this, firstEl_);
+    }
+    CircleIteratorConst& end() const
+    {
+        return CircleIteratorConst(*this, endEl_);
+    }
+    int indexOfFirst() const
+    {
+        return firstEl_;
     }
     void changeCapacity(int k)
     {
+        Value_T* n = new Value_T(k + 1);
+        int e = std::min((endEl_ - firstEl_ + capacity_) % capacity_, k);
+        for (int i = 0; i < e; i++)
+        {
+            n[i] = operator[](i);
+        }
         delete data_;
         capacity_ = k + 1;
         firstEl_ = 0;
-        endEl_ = 0;
-        data_ = new Value_T(capacity_);
+        endEl_ = e;
+        data_ = n;
         dataEnd_ = data_ + capacity_;
     }
 };
@@ -289,15 +363,12 @@ int main() {
         std::cout << ex[i] << "\n";
     }
     ex.changeCapacity(4);
-    for (int i = 0; i < 2; i++)
-    {
-        ex.push_front(i);
-        ex.push_back(i);
-    }
+
     std::cout << "\n";
     for (int i = 0; i < 4; i++)
     {
         std::cout << ex[i] << "\n";
     }
-    std::sort(ex.first(), ex.end());
+    CircleBuffer<int>::CircleIterator it(ex, 1);
+    CircleBuffer<int>::CircleIteratorConst itc(ex, 1);
 }
