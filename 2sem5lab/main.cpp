810ed83298711421cce47e14ff9ab8968ef5d648
yeti <crazyyeti@mail.ru>
2021-05-22 16:13:03 +0300

    5lab

--- 2sem5lab/main.cpp
+++ 2sem5lab/main.cpp
@@ -14,12 +14,12 @@ public:
     class CircleIterator: public std::iterator<std::random_access_iterator_tag, Value_T>
     {
     private:
-        CircleBuffer<Value_T>& buffer_;
+        CircleBuffer<Value_T>* buffer_ = NULL;
         int index_ = 0;
     public:
         CircleIterator() = default;
         CircleIterator(CircleBuffer<Value_T>& buffer, int index)
-        : buffer_(buffer),
+        : buffer_(&buffer),
         index_((index - buffer.indexOfFirst() + buffer.capasity()) % buffer.capasity())
         {}
         CircleIterator &operator=(const CircleIterator &other) = default;
@@ -36,13 +36,13 @@ public:
 
         CircleIterator &operator+=(int ind)
         {
-            index_ = ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
+            index_ = ((index_ + ind) % buffer_->capasity() + buffer_->capasity()) % buffer_->capasity();
             return *this;
         }
 
         CircleIterator &operator-=(int ind)
         {
-            index_ = ((index_ - ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity();
+            index_ = ((index_ - ind) % buffer_->capasity() + buffer_->capasity()) % buffer_->capasity();
             return *this;
         }
 
@@ -55,7 +55,7 @@ public:
 
         size_t operator-(const CircleIterator &other) const
         {
-            return ((index_ - other.index_ + buffer_.capasity()) % buffer_.capasity()) * sizeof(Value_T);
+            return ((index_ - other.index_ + buffer_->capasity()) % buffer_->capasity()) * sizeof(Value_T);
         }
 
         CircleIterator operator++()
@@ -93,7 +93,7 @@ public:
 
         Value_T &operator[](int ind)
         {
-            return *(buffer_.data() + ((index_ + ind) % buffer_.capasity() + buffer_.capasity()) % buffer_.capasity());
+            return *(buffer_->data() + ((index_ + ind) % buffer_->capasity() + buffer_->capasity()) % buffer_->capasity());
         }
 
         Value_T &operator*()
@@ -102,13 +102,13 @@ public:
         }
         bool operator> (const CircleIterator& other) const
         {
-            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >
-                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
+            return (index_ - buffer_->indexOfFirst() + buffer_->capacity_) % buffer_->capacity_ >
+                   (other.index_ - buffer_->indexOfFirst() + buffer_->capacity_) % buffer_->capacity_;
         }
         bool operator>= (const CircleIterator& other) const
         {
-            return (index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_ >=
-                   (other.index_ - buffer_.indexOfFirst() + buffer_.capacity_) % buffer_.capacity_;
+            return (index_ - buffer_->indexOfFirst() + buffer_->capacity_) % buffer_->capacity_ >=
+                   (other.index_ - buffer_->indexOfFirst() + buffer_->capacity_) % buffer_->capacity_;
         }
         bool operator< (const CircleIterator& other) const
         {
@@ -283,7 +283,7 @@ public:
                 return data_[k];
         throw std::invalid_argument("Out of range");
     }
-    Value_T& operator[](int index) const
+    const Value_T& operator[](int index) const
     {
         int k = (firstEl_ + index % capacity_ + capacity_) % capacity_;
         if ((endEl_ > firstEl_ && firstEl_ <= k && k < endEl_) ||
@@ -295,7 +295,7 @@ public:
     {
         return data_[firstEl_];
     }
-    Value_T& front() const
+    const Value_T& front() const
     {
         return data_[firstEl_];
     }
@@ -303,7 +303,7 @@ public:
     {
         return data_[endEl_ - 1];
     }
-    Value_T& back() const
+    const Value_T& back() const
     {
         return data_[endEl_ - 1];
     }
@@ -315,19 +315,19 @@ public:
     {
         return capacity_;
     }
-    CircleIterator& begin()
+    CircleIterator begin()
     {
         return CircleIterator(*this, firstEl_);
     }
-    CircleIterator& end()
+    CircleIterator end()
     {
         return CircleIterator(*this, endEl_);
     }
-    CircleIteratorConst& begin() const
+    CircleIteratorConst begin() const
     {
         return CircleIteratorConst(*this, firstEl_);
     }
-    CircleIteratorConst& end() const
+    CircleIteratorConst end() const
     {
         return CircleIteratorConst(*this, endEl_);
     }
@@ -371,4 +371,5 @@ int main() {
     }
     CircleBuffer<int>::CircleIterator it(ex, 1);
     CircleBuffer<int>::CircleIteratorConst itc(ex, 1);
+    CircleBuffer<int>::CircleIterator its;
 }
